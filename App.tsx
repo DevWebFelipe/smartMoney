5cc71c9d5b15e4980a71d590a9b14e2a88f8e156
Felipe Texeira <109564897+DevWebFelipe@users.noreply.github.com>
2024-01-04 21:54:21 -0300

    Criado esboço inicial da tela principal

--- App.tsx
+++ App.tsx
@@ -12,107 +12,74 @@ import {
   ScrollView,
   StatusBar,
   StyleSheet,
+  FlatList,
   Text,
+  Button,
   useColorScheme,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+function App(): React.JSX.Element {
+  addEntry = () => {
+    alert('Botão clicado');
+  };
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
   return (
-    <View style={styles.sectionContainer}>
+    <View style={{padding: 10}}>
       <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
+        style={{
+          fontSize: 22,
+          fontWeight: 'bold',
+          marginTop: 10,
+          marginBottom: 10,
+        }}>
+        Saldo: $2.102,45
       </Text>
+      <Button onPress={addEntry} title="Adicionar" />
+
       <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
+        style={{
+          fontSize: 22,
+          fontWeight: 'bold',
+          marginTop: 10,
+          marginBottom: 10,
+        }}>
+        Categorias
       </Text>
+
+      <FlatList
+        data={[
+          {key: 'Alimentação: $200,00'},
+          {key: 'Combustível: $12,00'},
+          {key: 'Aluguel: $120,00'},
+          {key: 'Lazer: $250,00'},
+          {key: 'Outros: $1.200,00'},
+        ]}
+        renderItem={({item}) => <Text>{item.key}</Text>}
+      />
+
+      <Text
+        style={{
+          fontSize: 22,
+          fontWeight: 'bold',
+          marginTop: 10,
+          marginBottom: 10,
+        }}>
+        Últimos lançamento
+      </Text>
+
+      <FlatList
+        data={[
+          {key: 'Padaria Asa Branca: $10,00'},
+          {key: 'Supermercado Isadora: $190,00'},
+          {key: 'Posto Ipiranga: $190,00'},
+        ]}
+        renderItem={({item}) => <Text>{item.key}</Text>}
+      />
     </View>
   );
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+const styles = StyleSheet.create({});
 
 export default App;
